81732caf2e4648b15bbb3572a6d82b14dd5577ad
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-21 19:25:52 -0700

    Mouse click on img elements is recptive. Now I need to learn how to make that do something.

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 'use strict';
 
 var productNumber = [];
+var lastThree = [];
 
-function Product(name, filepath, numberOfTimesShown, timesClicked) {
+function Product(name, filepath, timesShown, timesClicked) {
   this.name = name;
   this.filepath = filepath;
-  this.numberOfTimesShown = numberOfTimesShown;
+  this.numberOfTimesShown = timesShown;
   this.timesClicked = timesClicked;
   productNumber.push(this);
 };
-
-function randomNumber() {
-  Math.floor(Math.random() * productNumber.length);
-}
 
 var Bag = new Product('bag', 'img/bag.jpg', 0, 0);
 var Banana = new Product('banana', 'img/banana.jpg', 0, 0);
@@ -34,8 +31,6 @@ var Unicorn = new Product('unicorn', 'img/unicorn.jpg', 0, 0);
 var Usb = new Product('usb', 'img/usb.gif', 0, 0);
 var WaterCan = new Product('water-can', 'img/water-can.jpg', 0, 0);
 var WineGlass = new Product('wine-glass', 'img/wine-glass.jpg', 0, 0);
-
-
 
 function renderThreeProducts() {
   var imageOne = document.getElementById('one');
@@ -59,51 +54,27 @@ function renderThreeProducts() {
   };
   thirdImage.src = productNumber[randomThree].filepath;
   imageThree.appendChild(thirdImage);
+  lastThree = [];
+  lastThree.push(randomOne, randomTwo, randomThree);
 };
 
 renderThreeProducts();
 
-// function renderSecondProduct() {
-//   var imageTwo = document.getElementById('two');
-//   var secondImage = document.createElement('img');
-//   secondImage.src = productNumber[randomNumber].filepath;
-//   imageTwo.appendChild(secondImage);
-// };
-//
-// renderSecondProduct();
-//
-// function renderThirdProduct() {
-//   var imageThree = document.getElementById('three');
-//   var thirdImage = document.createElement('img');
-//   thirdImage.src = Bag.filepath;
-//   imageThree.appendChild(thirdImage);
-// };
-//
-// renderFirstProduct();
-// renderSecondProduct();
-// renderThirdProduct();
-//
-// function renderThree () {
-//   var imgOne = document.getElementById('one');
-//   var firstImg = document.createElement('img');
-//   var randomOne = Math.floor(Math.random() * productList.length);
-//   firstImg.src = productList[randomOne].path;
-//   imgOne.appendChild(firstImg);
-//   var imgTwo = document.getElementById('two');
-//   var secondImg = document.createElement('img');
-//   var randomTwo = Math.floor(Math.random() * productList.length);
-//   if (randomOne === randomTwo) {
-//     randomTwo = Math.floor(Math.random() * productList.length);
-//   } else {
-//     secondImg.src = productList[randomTwo].path;
-//     imgTwo.appendChild(secondImg);
-//     var imgThree = document.getElementById('three');
-//     var thirdImg = document.createElement('img');
-//     var randomThree = Math.floor(Math.random() * productList.length);
-//     if (randomThree === randomTwo || randomThree === randomOne) {
-//       randomThree = Math.floor(Math.random() * productList.length);
-//     }
-//     thirdImg.src = productList[randomThree].path;
-//     imgThree.appendChild(thirdImg);
-//   }
-// };
+function voteOne (event) {
+  console.log(event + ' this is the first object');
+}
+function voteTwo (event) {
+  console.log(event + ' this is the second object');
+}
+function voteThree (event) {
+  console.log(event + ' this is the third object');
+}
+
+var clickOne = document.getElementById('one');
+clickOne.addEventListener('click', voteOne);
+
+var clickTwo = document.getElementById('two');
+clickTwo.addEventListener('click', voteTwo);
+
+var clickThree = document.getElementById('three');
+clickThree.addEventListener('click', voteThree);
