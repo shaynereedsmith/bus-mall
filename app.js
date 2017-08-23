1236bc30198cad1e1bee2073c021ca176a24b84a
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-23 16:54:10 -0700

    saved number of clicks in local storage, and adjusts the value to persist

--- app.js
+++ app.js
@@ -22,26 +22,39 @@ function Product(name, filepath, id) {
   };
 };
 
-var Bag = new Product('R2D2 Bag', 'img/bag.jpg', 'bag');
-var Banana = new Product('Banana Slicer', 'img/banana.jpg', 'banana');
-var Bathroom = new Product('TP Tablet Holder', 'img/bathroom.jpg', 'bathroom');
-var Boots = new Product('Boots', 'img/boots.jpg', 'boots');
-var Breakfast = new Product('Easy-make Breakfast', 'img/breakfast.jpg', 'breakfast');
-var Bubblegum = new Product('Meatball Bubblegum', 'img/bubblegum.jpg', 'bubblegum');
-var Chair = new Product('A Chair', 'img/chair.jpg', 'chair');
-var Cthulhu = new Product('Cthulhu Plushy', 'img/cthulhu.jpg', 'cthulhu');
-var DogDuck = new Product('Duck Muzzle', 'img/dog-duck.jpg', 'dog-duck');
-var Dragon = new Product('Dragon Meat', 'img/dragon.jpg', 'dragon');
-var Pen = new Product('Pen Cutlery', 'img/pen.jpg', 'pen');
-var PetSweep = new Product('Pet Sweep', 'img/pet-sweep.jpg', 'pet-sweep');
-var Scissors = new Product('Pizza Scissors', 'img/scissors.jpg', 'scissors');
-var Shark = new Product('Shark Sleeping Bag', 'img/shark.jpg', 'shark');
-var Sweep = new Product('Baby Sweep', 'img/sweep.png', 'sweep');
-var Tauntaun = new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg', 'tauntaun');
-var Unicorn = new Product('Unicorn Meat', 'img/unicorn.jpg', 'unicorn');
-var Usb = new Product('Tentacle USB', 'img/usb.gif', 'usb');
-var WaterCan = new Product('Water Can', 'img/water-can.jpg', 'water-can');
-var WineGlass = new Product('Wine Glass', 'img/wine-glass.jpg', 'wine-glass');
+if (localStorage.productNumber) {
+  var productNumber = JSON.parse(localStorage.productNumber);
+} else {
+  var Bag = new Product('R2D2 Bag', 'img/bag.jpg', 'bag');
+  var Banana = new Product('Banana Slicer', 'img/banana.jpg', 'banana');
+  var Bathroom = new Product('TP Tablet Holder', 'img/bathroom.jpg', 'bathroom');
+  var Boots = new Product('Boots', 'img/boots.jpg', 'boots');
+  var Breakfast = new Product('Easy-make Breakfast', 'img/breakfast.jpg', 'breakfast');
+  var Bubblegum = new Product('Meatball Bubblegum', 'img/bubblegum.jpg', 'bubblegum');
+  var Chair = new Product('A Chair', 'img/chair.jpg', 'chair');
+  var Cthulhu = new Product('Cthulhu Plushy', 'img/cthulhu.jpg', 'cthulhu');
+  var DogDuck = new Product('Duck Muzzle', 'img/dog-duck.jpg', 'dog-duck');
+  var Dragon = new Product('Dragon Meat', 'img/dragon.jpg', 'dragon');
+  var Pen = new Product('Pen Cutlery', 'img/pen.jpg', 'pen');
+  var PetSweep = new Product('Pet Sweep', 'img/pet-sweep.jpg', 'pet-sweep');
+  var Scissors = new Product('Pizza Scissors', 'img/scissors.jpg', 'scissors');
+  var Shark = new Product('Shark Sleeping Bag', 'img/shark.jpg', 'shark');
+  var Sweep = new Product('Baby Sweep', 'img/sweep.png', 'sweep');
+  var Tauntaun = new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg', 'tauntaun');
+  var Unicorn = new Product('Unicorn Meat', 'img/unicorn.jpg', 'unicorn');
+  var Usb = new Product('Tentacle USB', 'img/usb.gif', 'usb');
+  var WaterCan = new Product('Water Can', 'img/water-can.jpg', 'water-can');
+  var WineGlass = new Product('Wine Glass', 'img/wine-glass.jpg', 'wine-glass');
+}
+
+var imageMaker = function(image) {
+  var anchor = document.getElementById('productSpace');
+  var img = document.createElement('img');
+  img.setAttribute('name', image.name);
+  img.setAttribute('src', image.filepath);
+  img.id = image.id;
+  anchor.appendChild(img);
+};
 
 var randomNumber = function() {
   return Math.floor(Math.random() * productNumber.length);
@@ -58,7 +71,7 @@ var renderThreeProducts = function() {
       continue;
     } else {
       currentThree.push(randomProduct);
-      randomProduct.imageMaker();
+      imageMaker(randomProduct);
     };
   };
   lastThree = currentThree;
@@ -80,6 +93,7 @@ function productSelector(event) {
           data.push(productNumber[i].timesClicked);
           name.push(productNumber[i].name);
         }
+        localStorage.setItem('productNumber', JSON.stringify(productNumber));
         chart(name, data);
       }
     }
